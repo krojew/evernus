cc7b4c34a9245b5189fc6f7c6046beae54c25eb2
Kamil Rojewski <devnull@localhost>
2014-06-21 16:06:03 +0200

    additional character fields

--- Character.cpp
+++ Character.cpp
@@ -32,6 +32,186 @@ namespace Evernus
         mKeyId = id;
     }
 
+    QString Character::getCorporationName() const &
+    {
+        return mCorporationName;
+    }
+
+    QString &&Character::getCorporationName() && noexcept
+    {
+        return std::move(mCorporationName);
+    }
+
+    void Character::setCorporationName(const QString &name)
+    {
+        mCorporationName = name;
+    }
+
+    void Character::setCorporationName(QString &&name)
+    {
+        mCorporationName = std::move(name);
+    }
+
+    QString Character::getRace() const &
+    {
+        return mRace;
+    }
+
+    QString &&Character::getRace() && noexcept
+    {
+        return std::move(mRace);
+    }
+
+    void Character::setRace(const QString &race)
+    {
+        mRace = race;
+    }
+
+    void Character::setRace(QString &&race)
+    {
+        mRace = std::move(race);
+    }
+
+    QString Character::getBloodline() const &
+    {
+        return mBloodline;
+    }
+
+    QString &&Character::getBloodline() && noexcept
+    {
+        return std::move(mBloodline);
+    }
+
+    void Character::setBloodline(const QString &bloodline)
+    {
+        mBloodline = bloodline;
+    }
+
+    void Character::setBloodline(QString &&bloodline)
+    {
+        mBloodline = std::move(bloodline);
+    }
+
+    QString Character::getAncestry() const &
+    {
+        return mAncestry;
+    }
+
+    QString &&Character::getAncestry() && noexcept
+    {
+        return std::move(mAncestry);
+    }
+
+    void Character::setAncestry(const QString &ancestry)
+    {
+        mAncestry = ancestry;
+    }
+
+    void Character::setAncestry(QString &&ancestry)
+    {
+        mAncestry = std::move(ancestry);
+    }
+
+    QString Character::getGender() const &
+    {
+        return mGender;
+    }
+
+    QString &&Character::getGender() && noexcept
+    {
+        return std::move(mGender);
+    }
+
+    void Character::setGender(const QString &gender)
+    {
+        mGender = gender;
+    }
+
+    void Character::setGender(QString &&gender)
+    {
+        mGender = std::move(gender);
+    }
+
+    uint Character::getSkillPoints() const noexcept
+    {
+        return mSkillPoints;
+    }
+
+    void Character::setSkillPoints(uint points) noexcept
+    {
+        mSkillPoints = points;
+    }
+
+    Character::ISKType Character::getISK() const
+    {
+        return mISK;
+    }
+
+    void Character::setISK(ISKType isk)
+    {
+        mISK = isk;
+    }
+
+    Character::OrderAmountSkills Character::getOrderAmountSkills() const noexcept
+    {
+        return mOrderAmountSkills;
+    }
+
+    void Character::setOrderAmountSkills(const OrderAmountSkills &skills) noexcept
+    {
+        mOrderAmountSkills = skills;
+    }
+
+    void Character::setOrderAmountSkills(OrderAmountSkills &&skills) noexcept
+    {
+        mOrderAmountSkills = std::move(skills);
+    }
+
+    Character::TradeRangeSkills Character::getTradeRangeSkills() const noexcept
+    {
+        return mTradeRangeSkills;
+    }
+
+    void Character::setTradeRangeSkills(const TradeRangeSkills &skills) noexcept
+    {
+        mTradeRangeSkills = skills;
+    }
+
+    void Character::setTradeRangeSkills(TradeRangeSkills &&skills) noexcept
+    {
+        mTradeRangeSkills = std::move(skills);
+    }
+
+    Character::FeeSkills Character::getFeeSkills() const noexcept
+    {
+        return mFeeSkills;
+    }
+
+    void Character::setFeeSkills(const FeeSkills &skills) noexcept
+    {
+        mFeeSkills = skills;
+    }
+
+    void Character::setFeeSkills(FeeSkills &&skills) noexcept
+    {
+        mFeeSkills = std::move(skills);
+    }
+
+    Character::ContractSkills Character::getContractSkills() const noexcept
+    {
+        return mContractSkills;
+    }
+
+    void Character::setContractSkills(const ContractSkills &skills) noexcept
+    {
+        mContractSkills = skills;
+    }
+
+    void Character::setContractSkills(ContractSkills &&skills) noexcept
+    {
+        mContractSkills = std::move(skills);
+    }
+
     bool Character::isEnabled() const noexcept
     {
         return mEnabled;
--- Character.h
+++ Character.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <boost/multiprecision/cpp_dec_float.hpp>
 #include <boost/optional.hpp>
 
 #include <QString>
@@ -13,6 +14,36 @@ namespace Evernus
     {
     public:
         typedef boost::optional<Key::IdType> KeyIdType;
+        typedef boost::multiprecision::cpp_dec_float_50 ISKType;
+
+        struct OrderAmountSkills
+        {
+            int mTrade = 0;
+            int mRetail = 0;
+            int mWholesale = 0;
+            int mTycoon = 0;
+        };
+
+        struct TradeRangeSkills
+        {
+            int mMarketing = 0;
+            int mProcurement = 0;
+            int mDaytrading = 0;
+            int mVisibility = 0;
+        };
+
+        struct FeeSkills
+        {
+            int mAccounting = 0;
+            int mBrokerRelations = 0;
+            int mMarginTrading = 0;
+        };
+
+        struct ContractSkills
+        {
+            int mContracting = 0;
+            int mCorporationContracting = 0;
+        };
 
         using Entity::Entity;
         virtual ~Character() = default;
@@ -25,12 +56,72 @@ namespace Evernus
         KeyIdType getKeyId() const noexcept;
         void setKeyId(Key::IdType id);
 
+        QString getCorporationName() const &;
+        QString &&getCorporationName() && noexcept;
+        void setCorporationName(const QString &name);
+        void setCorporationName(QString &&name);
+
+        QString getRace() const &;
+        QString &&getRace() && noexcept;
+        void setRace(const QString &race);
+        void setRace(QString &&race);
+
+        QString getBloodline() const &;
+        QString &&getBloodline() && noexcept;
+        void setBloodline(const QString &bloodline);
+        void setBloodline(QString &&bloodline);
+
+        QString getAncestry() const &;
+        QString &&getAncestry() && noexcept;
+        void setAncestry(const QString &ancestry);
+        void setAncestry(QString &&ancestry);
+
+        QString getGender() const &;
+        QString &&getGender() && noexcept;
+        void setGender(const QString &gender);
+        void setGender(QString &&gender);
+
+        uint getSkillPoints() const noexcept;
+        void setSkillPoints(uint points) noexcept;
+
+        ISKType getISK() const;
+        void setISK(ISKType isk);
+
+        OrderAmountSkills getOrderAmountSkills() const noexcept;
+        void setOrderAmountSkills(const OrderAmountSkills &skills) noexcept;
+        void setOrderAmountSkills(OrderAmountSkills &&skills) noexcept;
+
+        TradeRangeSkills getTradeRangeSkills() const noexcept;
+        void setTradeRangeSkills(const TradeRangeSkills &skills) noexcept;
+        void setTradeRangeSkills(TradeRangeSkills &&skills) noexcept;
+
+        FeeSkills getFeeSkills() const noexcept;
+        void setFeeSkills(const FeeSkills &skills) noexcept;
+        void setFeeSkills(FeeSkills &&skills) noexcept;
+
+        ContractSkills getContractSkills() const noexcept;
+        void setContractSkills(const ContractSkills &skills) noexcept;
+        void setContractSkills(ContractSkills &&skills) noexcept;
+
         bool isEnabled() const noexcept;
         void setEnabled(bool flag) noexcept;
 
     private:
         QString mName;
         KeyIdType mKeyId;
+        QString mCorporationName;
+        QString mRace;
+        QString mBloodline;
+        QString mAncestry;
+        QString mGender;
+        uint mSkillPoints = 0;
+        ISKType mISK = 0;
+
+        OrderAmountSkills mOrderAmountSkills;
+        TradeRangeSkills mTradeRangeSkills;
+        FeeSkills mFeeSkills;
+        ContractSkills mContractSkills;
+
         bool mEnabled = true;
     };
 }
