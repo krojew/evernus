3173f58dc9956aaf6e89246f7847b5dabe86d367
krojew <devnull@localhost>
2014-08-21 14:47:34 +0200

    version bump

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
     try
     {
         QCoreApplication::setApplicationName("Evernus");
-        QCoreApplication::setApplicationVersion("1.8");
+        QCoreApplication::setApplicationVersion("1.9");
         QCoreApplication::setOrganizationDomain("evernus.com");
         QCoreApplication::setOrganizationName("evernus.com");
 
