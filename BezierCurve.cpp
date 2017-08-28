0147d471046ee9a03388ecd23eb417dfa5ef87ce
krojew <devnull@localhost>
2017-08-28 08:39:43 +0200

    little refactoring

--- BezierCurve.cpp
+++ BezierCurve.cpp
@@ -53,106 +53,114 @@
 #include <QtQuick/qsgnode.h>
 #include <QtQuick/qsgflatcolormaterial.h>
 
-BezierCurve::BezierCurve(QQuickItem *parent)
-    : QQuickItem(parent)
-    , m_p1(0, 0)
-    , m_p2(1, 0)
-    , m_p3(0, 1)
-    , m_p4(1, 1)
-    , m_segmentCount(32)
+namespace Evernus
 {
-    setFlag(ItemHasContents, true);
-}
-
-void BezierCurve::setP1(const QPointF &p)
-{
-    if (p == m_p1)
-        return;
-
-    m_p1 = p;
-    emit p1Changed(p);
-    update();
-}
-
-void BezierCurve::setP2(const QPointF &p)
-{
-    if (p == m_p2)
-        return;
-
-    m_p2 = p;
-    emit p2Changed(p);
-    update();
-}
-
-void BezierCurve::setP3(const QPointF &p)
-{
-    if (p == m_p3)
-        return;
-
-    m_p3 = p;
-    emit p3Changed(p);
-    update();
-}
-
-void BezierCurve::setP4(const QPointF &p)
-{
-    if (p == m_p4)
-        return;
-
-    m_p4 = p;
-    emit p4Changed(p);
-    update();
-}
-
-void BezierCurve::setSegmentCount(int count)
-{
-    if (m_segmentCount == count)
-        return;
-
-    m_segmentCount = count;
-    emit segmentCountChanged(count);
-    update();
-}
-
-QSGNode *BezierCurve::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *)
-{
-    QSGGeometryNode *node = 0;
-    QSGGeometry *geometry = 0;
-
-    if (!oldNode) {
-        node = new QSGGeometryNode;
-        geometry = new QSGGeometry(QSGGeometry::defaultAttributes_Point2D(), m_segmentCount);
-        geometry->setLineWidth(2);
-        geometry->setDrawingMode(QSGGeometry::DrawLineStrip);
-        node->setGeometry(geometry);
-        node->setFlag(QSGNode::OwnsGeometry);
-        QSGFlatColorMaterial *material = new QSGFlatColorMaterial;
-        material->setColor(QColor(255, 0, 0));
-        node->setMaterial(material);
-        node->setFlag(QSGNode::OwnsMaterial);
-    } else {
-        node = static_cast<QSGGeometryNode *>(oldNode);
-        geometry = node->geometry();
-        geometry->allocate(m_segmentCount);
+    BezierCurve::BezierCurve(QQuickItem *parent)
+        : QQuickItem(parent)
+        , m_p1(0, 0)
+        , m_p2(1, 0)
+        , m_p3(0, 1)
+        , m_p4(1, 1)
+    {
+        setFlag(ItemHasContents, true);
     }
 
-    QRectF bounds = boundingRect();
-    QSGGeometry::Point2D *vertices = geometry->vertexDataAsPoint2D();
-    for (int i = 0; i < m_segmentCount; ++i) {
-        qreal t = i / qreal(m_segmentCount - 1);
-        qreal invt = 1 - t;
+    void BezierCurve::setP1(const QPointF &p)
+    {
+        if (p == m_p1)
+            return;
 
-        QPointF pos = invt * invt * invt * m_p1
-                    + 3 * invt * invt * t * m_p2
-                    + 3 * invt * t * t * m_p3
-                    + t * t * t * m_p4;
-
-        float x = bounds.x() + pos.x() * bounds.width();
-        float y = bounds.y() + pos.y() * bounds.height();
-
-        vertices[i].set(x, y);
+        m_p1 = p;
+        emit p1Changed(p);
+        update();
     }
-    node->markDirty(QSGNode::DirtyGeometry);
 
-    return node;
+    void BezierCurve::setP2(const QPointF &p)
+    {
+        if (p == m_p2)
+            return;
+
+        m_p2 = p;
+        emit p2Changed(p);
+        update();
+    }
+
+    void BezierCurve::setP3(const QPointF &p)
+    {
+        if (p == m_p3)
+            return;
+
+        m_p3 = p;
+        emit p3Changed(p);
+        update();
+    }
+
+    void BezierCurve::setP4(const QPointF &p)
+    {
+        if (p == m_p4)
+            return;
+
+        m_p4 = p;
+        emit p4Changed(p);
+        update();
+    }
+
+    void BezierCurve::setSegmentCount(int count)
+    {
+        if (m_segmentCount == count)
+            return;
+
+        m_segmentCount = count;
+        emit segmentCountChanged(count);
+        update();
+    }
+
+    QSGNode *BezierCurve::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *)
+    {
+        QSGGeometryNode *node = nullptr;
+        QSGGeometry *geometry = nullptr;
+
+        if (oldNode == nullptr)
+        {
+            node = new QSGGeometryNode;
+            geometry = new QSGGeometry(QSGGeometry::defaultAttributes_Point2D(), m_segmentCount);
+            geometry->setLineWidth(2);
+            geometry->setDrawingMode(QSGGeometry::DrawLineStrip);
+            node->setGeometry(geometry);
+            node->setFlag(QSGNode::OwnsGeometry);
+
+            const auto material = new QSGFlatColorMaterial;
+            material->setColor(QColor(255, 0, 0));
+            node->setMaterial(material);
+            node->setFlag(QSGNode::OwnsMaterial);
+        }
+        else
+        {
+            node = static_cast<QSGGeometryNode *>(oldNode);
+            geometry = node->geometry();
+            geometry->allocate(m_segmentCount);
+        }
+
+        const auto bounds = boundingRect();
+        const auto vertices = geometry->vertexDataAsPoint2D();
+
+        for (auto i = 0; i < m_segmentCount; ++i)
+        {
+            const auto t = i / qreal(m_segmentCount - 1);
+            const auto invt = 1 - t;
+
+            const auto pos = invt * invt * invt * m_p1
+                             + 3 * invt * invt * t * m_p2
+                             + 3 * invt * t * t * m_p3
+                             + t * t * t * m_p4;
+
+            float x = bounds.x() + pos.x() * bounds.width();
+            float y = bounds.y() + pos.y() * bounds.height();
+
+            vertices[i].set(x, y);
+        }
+
+        node->markDirty(QSGNode::DirtyGeometry);
+        return node;
+    }
 }
--- BezierCurve.h
+++ BezierCurve.h
@@ -51,50 +51,54 @@
 
 #include <QQuickItem>
 
-class BezierCurve : public QQuickItem
+namespace Evernus
 {
-    Q_OBJECT
+    class BezierCurve
+        : public QQuickItem
+    {
+        Q_OBJECT
 
-    Q_PROPERTY(QPointF p1 READ p1 WRITE setP1 NOTIFY p1Changed)
-    Q_PROPERTY(QPointF p2 READ p2 WRITE setP2 NOTIFY p2Changed)
-    Q_PROPERTY(QPointF p3 READ p3 WRITE setP3 NOTIFY p3Changed)
-    Q_PROPERTY(QPointF p4 READ p4 WRITE setP4 NOTIFY p4Changed)
+        Q_PROPERTY(QPointF p1 READ p1 WRITE setP1 NOTIFY p1Changed)
+        Q_PROPERTY(QPointF p2 READ p2 WRITE setP2 NOTIFY p2Changed)
+        Q_PROPERTY(QPointF p3 READ p3 WRITE setP3 NOTIFY p3Changed)
+        Q_PROPERTY(QPointF p4 READ p4 WRITE setP4 NOTIFY p4Changed)
 
-    Q_PROPERTY(int segmentCount READ segmentCount WRITE setSegmentCount NOTIFY segmentCountChanged)
+        Q_PROPERTY(int segmentCount READ segmentCount WRITE setSegmentCount NOTIFY segmentCountChanged)
 
-public:
-    BezierCurve(QQuickItem *parent = 0);
-    virtual ~BezierCurve() = default;
+    public:
+        explicit BezierCurve(QQuickItem *parent = nullptr);
+        virtual ~BezierCurve() = default;
 
-    QSGNode *updatePaintNode(QSGNode *, UpdatePaintNodeData *);
+        QSGNode *updatePaintNode(QSGNode *, UpdatePaintNodeData *);
 
-    QPointF p1() const { return m_p1; }
-    QPointF p2() const { return m_p2; }
-    QPointF p3() const { return m_p3; }
-    QPointF p4() const { return m_p4; }
+        QPointF p1() const { return m_p1; }
+        QPointF p2() const { return m_p2; }
+        QPointF p3() const { return m_p3; }
+        QPointF p4() const { return m_p4; }
 
-    int segmentCount() const { return m_segmentCount; }
+        int segmentCount() const { return m_segmentCount; }
 
-    void setP1(const QPointF &p);
-    void setP2(const QPointF &p);
-    void setP3(const QPointF &p);
-    void setP4(const QPointF &p);
+        void setP1(const QPointF &p);
+        void setP2(const QPointF &p);
+        void setP3(const QPointF &p);
+        void setP4(const QPointF &p);
 
-    void setSegmentCount(int count);
+        void setSegmentCount(int count);
 
-signals:
-    void p1Changed(const QPointF &p);
-    void p2Changed(const QPointF &p);
-    void p3Changed(const QPointF &p);
-    void p4Changed(const QPointF &p);
+    signals:
+        void p1Changed(const QPointF &p);
+        void p2Changed(const QPointF &p);
+        void p3Changed(const QPointF &p);
+        void p4Changed(const QPointF &p);
 
-    void segmentCountChanged(int count);
+        void segmentCountChanged(int count);
 
-private:
-    QPointF m_p1;
-    QPointF m_p2;
-    QPointF m_p3;
-    QPointF m_p4;
+    private:
+        QPointF m_p1;
+        QPointF m_p2;
+        QPointF m_p3;
+        QPointF m_p4;
 
-    int m_segmentCount;
-};
+        int m_segmentCount = 32;
+    };
+}
