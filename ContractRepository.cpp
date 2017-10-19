1d46859842c45e3aa019b2294480e1e2671031d4
krojew <devnull@localhost>
2017-10-19 11:30:18 +0200

    string literals

--- ContractRepository.cpp
+++ ContractRepository.cpp
@@ -33,50 +33,50 @@ namespace Evernus
 
     QString ContractRepository::getTableName() const
     {
-        return (mCorp) ? ("corp_contracts") : ("contracts");
+        return (mCorp) ? (QStringLiteral("corp_contracts")) : (QStringLiteral("contracts"));
     }
 
     QString ContractRepository::getIdColumn() const
     {
-        return "id";
+        return QStringLiteral("id");
     }
 
     ContractRepository::EntityPtr ContractRepository::populate(const QSqlRecord &record) const
     {
-        auto issued = record.value("issued").toDateTime();
+        auto issued = record.value(QStringLiteral("issued")).toDateTime();
         issued.setTimeSpec(Qt::UTC);
 
-        auto expired = record.value("expired").toDateTime();
+        auto expired = record.value(QStringLiteral("expired")).toDateTime();
         expired.setTimeSpec(Qt::UTC);
 
-        auto accepted = record.value("accepted").toDateTime();
+        auto accepted = record.value(QStringLiteral("accepted")).toDateTime();
         accepted.setTimeSpec(Qt::UTC);
 
-        auto completed = record.value("completed").toDateTime();
+        auto completed = record.value(QStringLiteral("completed")).toDateTime();
         completed.setTimeSpec(Qt::UTC);
 
-        auto contract = std::make_shared<Contract>(record.value("id").value<Contract::IdType>());
-        contract->setIssuerId(record.value("issuer_id").value<Character::IdType>());
-        contract->setIssuerCorpId(record.value("issuer_corp_id").toULongLong());
-        contract->setAssigneeId(record.value("assignee_id").toULongLong());
-        contract->setAcceptorId(record.value("acceptor_id").toULongLong());
-        contract->setStartStationId(record.value("start_station_id").toUInt());
-        contract->setEndStationId(record.value("end_station_id").toUInt());
-        contract->setType(static_cast<Contract::Type>(record.value("type").toInt()));
-        contract->setStatus(static_cast<Contract::Status>(record.value("status").toInt()));
-        contract->setTitle(record.value("title").toString());
-        contract->setForCorp(record.value("for_corp").toBool());
-        contract->setAvailability(static_cast<Contract::Availability>(record.value("availability").toInt()));
+        auto contract = std::make_shared<Contract>(record.value(QStringLiteral("id")).value<Contract::IdType>());
+        contract->setIssuerId(record.value(QStringLiteral("issuer_id")).value<Character::IdType>());
+        contract->setIssuerCorpId(record.value(QStringLiteral("issuer_corp_id")).toULongLong());
+        contract->setAssigneeId(record.value(QStringLiteral("assignee_id")).toULongLong());
+        contract->setAcceptorId(record.value(QStringLiteral("acceptor_id")).toULongLong());
+        contract->setStartStationId(record.value(QStringLiteral("start_station_id")).toUInt());
+        contract->setEndStationId(record.value(QStringLiteral("end_station_id")).toUInt());
+        contract->setType(static_cast<Contract::Type>(record.value(QStringLiteral("type")).toInt()));
+        contract->setStatus(static_cast<Contract::Status>(record.value(QStringLiteral("status")).toInt()));
+        contract->setTitle(record.value(QStringLiteral("title")).toString());
+        contract->setForCorp(record.value(QStringLiteral("for_corp")).toBool());
+        contract->setAvailability(static_cast<Contract::Availability>(record.value(QStringLiteral("availability")).toInt()));
         contract->setIssued(issued);
         contract->setExpired(expired);
         contract->setAccepted(accepted);
         contract->setCompleted(completed);
-        contract->setNumDays(record.value("num_days").toInt());
-        contract->setPrice(record.value("price").toDouble());
-        contract->setReward(record.value("reward").toDouble());
-        contract->setCollateral(record.value("collateral").toDouble());
-        contract->setBuyout(record.value("buyout").toDouble());
-        contract->setVolume(record.value("volume").toDouble());
+        contract->setNumDays(record.value(QStringLiteral("num_days")).toInt());
+        contract->setPrice(record.value(QStringLiteral("price")).toDouble());
+        contract->setReward(record.value(QStringLiteral("reward")).toDouble());
+        contract->setCollateral(record.value(QStringLiteral("collateral")).toDouble());
+        contract->setBuyout(record.value(QStringLiteral("buyout")).toDouble());
+        contract->setVolume(record.value(QStringLiteral("volume")).toDouble());
         contract->setNew(false);
 
         return contract;
@@ -84,7 +84,7 @@ namespace Evernus
 
     void ContractRepository::create() const
     {
-        exec(QString{"CREATE TABLE IF NOT EXISTS %1 ("
+        exec(QStringLiteral("CREATE TABLE IF NOT EXISTS %1 (QStringLiteral("
             "id BIGINT PRIMARY KEY,"
             "issuer_id BIGINT NOT NULL,"
             "issuer_corp_id BIGINT NOT NULL,"
@@ -107,86 +107,86 @@ namespace Evernus
             "collateral DOUBLE NOT NULL,"
             "buyout DOUBLE NOT NULL,"
             "volume DOUBLE NOT NULL"
-        ")"}.arg(getTableName()));
+        "))").arg(getTableName()));
 
-        exec(QString{"CREATE INDEX IF NOT EXISTS %1_issuer ON %1(issuer_id)"}.arg(getTableName()));
-        exec(QString{"CREATE INDEX IF NOT EXISTS %1_issuer_corp ON %1(issuer_corp_id)"}.arg(getTableName()));
-        exec(QString{"CREATE INDEX IF NOT EXISTS %1_assignee ON %1(assignee_id)"}.arg(getTableName()));
+        exec(QStringLiteral("CREATE INDEX IF NOT EXISTS %1_issuer ON %1(issuer_id)").arg(getTableName()));
+        exec(QStringLiteral("CREATE INDEX IF NOT EXISTS %1_issuer_corp ON %1(issuer_corp_id)").arg(getTableName()));
+        exec(QStringLiteral("CREATE INDEX IF NOT EXISTS %1_assignee ON %1(assignee_id)").arg(getTableName()));
     }
 
     ContractRepository::EntityList ContractRepository::fetchIssuedForCharacter(Character::IdType id) const
     {
-        return fetchByColumnWithItems(id, "issuer_id");
+        return fetchByColumnWithItems(id, QStringLiteral("issuer_id"));
     }
 
     ContractRepository::EntityList ContractRepository::fetchAssignedForCharacter(Character::IdType id) const
     {
-        return fetchByColumnWithItems(id, "assignee_id");
+        return fetchByColumnWithItems(id, QStringLiteral("assignee_id"));
     }
 
     ContractRepository::EntityList ContractRepository::fetchIssuedForCorporation(quint64 id) const
     {
-        return fetchByColumnWithItems(id, "issuer_corp_id");
+        return fetchByColumnWithItems(id, QStringLiteral("issuer_corp_id"));
     }
 
     ContractRepository::EntityList ContractRepository::fetchAssignedForCorporation(quint64 id) const
     {
-        return fetchByColumnWithItems(id, "assignee_id");
+        return fetchByColumnWithItems(id, QStringLiteral("assignee_id"));
     }
 
     QStringList ContractRepository::getColumns() const
     {
         return QStringList{}
-            << "id"
-            << "issuer_id"
-            << "issuer_corp_id"
-            << "assignee_id"
-            << "acceptor_id"
-            << "start_station_id"
-            << "end_station_id"
-            << "type"
-            << "status"
-            << "title"
-            << "for_corp"
-            << "availability"
-            << "issued"
-            << "expired"
-            << "accepted"
-            << "completed"
-            << "num_days"
-            << "price"
-            << "reward"
-            << "collateral"
-            << "buyout"
-            << "volume";
+            << QStringLiteral("id")
+            << QStringLiteral("issuer_id")
+            << QStringLiteral("issuer_corp_id")
+            << QStringLiteral("assignee_id")
+            << QStringLiteral("acceptor_id")
+            << QStringLiteral("start_station_id")
+            << QStringLiteral("end_station_id")
+            << QStringLiteral("type")
+            << QStringLiteral("status")
+            << QStringLiteral("title")
+            << QStringLiteral("for_corp")
+            << QStringLiteral("availability")
+            << QStringLiteral("issued")
+            << QStringLiteral("expired")
+            << QStringLiteral("accepted")
+            << QStringLiteral("completed")
+            << QStringLiteral("num_days")
+            << QStringLiteral("price")
+            << QStringLiteral("reward")
+            << QStringLiteral("collateral")
+            << QStringLiteral("buyout")
+            << QStringLiteral("volume");
     }
 
     void ContractRepository::bindValues(const Contract &entity, QSqlQuery &query) const
     {
         if (entity.getId() != Contract::invalidId)
-            query.bindValue(":id", entity.getId());
+            query.bindValue(QStringLiteral(":id"), entity.getId());
 
-        query.bindValue(":issuer_id", entity.getIssuerId());
-        query.bindValue(":issuer_corp_id", entity.getIssuerCorpId());
-        query.bindValue(":assignee_id", entity.getAssigneeId());
-        query.bindValue(":acceptor_id", entity.getAcceptorId());
-        query.bindValue(":start_station_id", entity.getStartStationId());
-        query.bindValue(":end_station_id", entity.getEndStationId());
-        query.bindValue(":type", static_cast<int>(entity.getType()));
-        query.bindValue(":status", static_cast<int>(entity.getStatus()));
-        query.bindValue(":title", entity.getTitle());
-        query.bindValue(":for_corp", entity.isForCorp());
-        query.bindValue(":availability", static_cast<int>(entity.getAvailability()));
-        query.bindValue(":issued", entity.getIssued());
-        query.bindValue(":expired", entity.getExpired());
-        query.bindValue(":accepted", entity.getAccepted());
-        query.bindValue(":completed", entity.getCompleted());
-        query.bindValue(":num_days", entity.getNumDays());
-        query.bindValue(":price", entity.getPrice());
-        query.bindValue(":reward", entity.getReward());
-        query.bindValue(":collateral", entity.getCollateral());
-        query.bindValue(":buyout", entity.getBuyout());
-        query.bindValue(":volume", entity.getVolume());
+        query.bindValue(QStringLiteral(":issuer_id"), entity.getIssuerId());
+        query.bindValue(QStringLiteral(":issuer_corp_id"), entity.getIssuerCorpId());
+        query.bindValue(QStringLiteral(":assignee_id"), entity.getAssigneeId());
+        query.bindValue(QStringLiteral(":acceptor_id"), entity.getAcceptorId());
+        query.bindValue(QStringLiteral(":start_station_id"), entity.getStartStationId());
+        query.bindValue(QStringLiteral(":end_station_id"), entity.getEndStationId());
+        query.bindValue(QStringLiteral(":type"), static_cast<int>(entity.getType()));
+        query.bindValue(QStringLiteral(":status"), static_cast<int>(entity.getStatus()));
+        query.bindValue(QStringLiteral(":title"), entity.getTitle());
+        query.bindValue(QStringLiteral(":for_corp"), entity.isForCorp());
+        query.bindValue(QStringLiteral(":availability"), static_cast<int>(entity.getAvailability()));
+        query.bindValue(QStringLiteral(":issued"), entity.getIssued());
+        query.bindValue(QStringLiteral(":expired"), entity.getExpired());
+        query.bindValue(QStringLiteral(":accepted"), entity.getAccepted());
+        query.bindValue(QStringLiteral(":completed"), entity.getCompleted());
+        query.bindValue(QStringLiteral(":num_days"), entity.getNumDays());
+        query.bindValue(QStringLiteral(":price"), entity.getPrice());
+        query.bindValue(QStringLiteral(":reward"), entity.getReward());
+        query.bindValue(QStringLiteral(":collateral"), entity.getCollateral());
+        query.bindValue(QStringLiteral(":buyout"), entity.getBuyout());
+        query.bindValue(QStringLiteral(":volume"), entity.getVolume());
     }
 
     void ContractRepository::bindPositionalValues(const Contract &entity, QSqlQuery &query) const
@@ -227,16 +227,16 @@ namespace Evernus
     ContractRepository::EntityList ContractRepository::fetchByColumnWithItems(T id, const QString &column) const
     {
         // all this because https://bugreports.qt-project.org/browse/QTBUG-14904
-        auto queryStr = QString{"SELECT %1.*, %4 FROM %1 LEFT JOIN %3 i ON i.contract_id = %1.id WHERE %1.%2 = ?"}
+        auto queryStr = QStringLiteral("SELECT %1.*, %4 FROM %1 LEFT JOIN %3 i ON i.contract_id = %1.id WHERE %1.%2 = ?")
             .arg(getTableName())
             .arg(column)
             .arg(mContractItemRepo.getTableName());
 
         auto columns = mContractItemRepo.getColumns();
         for (auto &column : columns)
-            column = QString("i.%1 i_%1").arg(column);
+            column = QString(QStringLiteral("i.%1 i_%1")).arg(column);
 
-        queryStr = queryStr.arg(columns.join(", "));
+        queryStr = queryStr.arg(columns.join(QStringLiteral(", ")));
 
         auto query = prepare(queryStr);
         query.bindValue(0, id);
@@ -253,13 +253,13 @@ namespace Evernus
 
         while (query.next())
         {
-            const auto id = query.value("id").value<Contract::IdType>();
+            const auto id = query.value(QStringLiteral("id")).template value<Contract::IdType>();
             auto it = contracts.find(id);
             if (it == std::end(contracts))
                 it = contracts.emplace(id, populate(query.record())).first;
 
-            if (!query.isNull("i_id"))
-                it->second->addItem(mContractItemRepo.populate("i_", query.record()));
+            if (!query.isNull(QStringLiteral("i_id")))
+                it->second->addItem(mContractItemRepo.populate(QStringLiteral("i_"), query.record()));
         }
 
         for (auto &contract : contracts)
