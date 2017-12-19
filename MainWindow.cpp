83b4c91db0abc1083751a2553cb76b8ada839ff3
Kamil Rojewski <devnull@localhost>
2017-12-19 19:08:01 +0100

    new scopes

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -426,8 +426,11 @@ namespace Evernus
                 "esi-markets.read_character_orders.v1 "
                 "esi-characters.read_blueprints.v1 "
                 "esi-contracts.read_character_contracts.v1 "
+                "esi-wallet.read_corporation_wallets.v1 "
                 "esi-assets.read_corporation_assets.v1 "
-                "esi-contracts.read_corporation_contracts.v1 "
+                "esi-corporations.read_blueprints.v1 "
+                "esi-contracts.read_corporation_contracts.v1"
+                "esi-markets.read_corporation_orders.v1  "
                 "esi-industry.read_character_mining.v1"
             )
         );
