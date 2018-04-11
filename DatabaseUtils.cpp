de3a39d02a4aa221027fd7c3cb5541cb1084d344
Kamil Rojewski <devnull@localhost>
2018-04-11 18:29:18 +0200

    SQLITE_LOCKED hack
    refs #437

--- DatabaseUtils.cpp
+++ DatabaseUtils.cpp
@@ -13,6 +13,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <stdexcept>
+#include <thread>
+#include <chrono>
 
 #include <boost/throw_exception.hpp>
 
@@ -25,6 +27,8 @@
 #include <QFile>
 
 #include "DatabaseUtils.h"
+
+using namespace std::literals;
 
 namespace Evernus
 {
@@ -45,10 +49,36 @@ namespace Evernus
             qDebug() << "SQL:" << query.lastQuery();
             if (!query.exec())
             {
-                const auto error = query.lastError().text();
+                auto error = query.lastError();
 
                 qCritical() << error;
-                BOOST_THROW_EXCEPTION(std::runtime_error{error.toStdString()});
+
+                // special case for SQLITE_LOCKED - it happens, but according to official docs, it shouldn't (we have a connection per thread and no shared cache)
+                // this means the docs are incomplete, so we just do the most awful thing imaginable - retry until dead (no access to underlying sqlite api to make it work)
+
+                const auto SQLITE_LOCKED = QStringLiteral("6");
+                if (error.nativeErrorCode() == SQLITE_LOCKED)
+                {
+                    qInfo() << "Engaging Horrible SQLITE_LOCKED Hack!";
+
+                    auto counter = 100;
+                    do {
+                        std::this_thread::sleep_for(100ms);
+
+                        if (query.exec())
+                            return;
+
+                        error = query.lastError();
+                        qCritical() << error;
+
+                        if (error.nativeErrorCode() != SQLITE_LOCKED)
+                            break;
+
+                        --counter;
+                    } while (counter > 0);
+                }
+
+                BOOST_THROW_EXCEPTION(std::runtime_error{error.text().toStdString()});
             }
         }
 
